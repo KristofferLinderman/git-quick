6725c9083ef4cd3b4a0ae9ff9067dbcc992ba292
Kristoffer Linderman <kristoffer.linderman@bygghemma.se>
2023-10-21 16:04:09 +0200

    feat: add basic cherry btn

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,13 +1,36 @@
-console.log('content loaded');
-
 const commitItems = document.querySelectorAll('.TimelineItem-body > ol > li');
-console.log('🚀 ~ file: index.ts:4 ~ commitItems:', commitItems);
 
+const createBtn = (commitItem: HTMLLIElement) => {
+  const commitSHA = commitItem.dataset.url?.split('commits/')[1].split('/')[0];
+  const cherryBtn = document.createElement('button');
+
+  cherryBtn.style.padding = '0';
+  cherryBtn.style.border = '1px solid rgba(31, 35, 40, 0.15)';
+  cherryBtn.style.borderRadius = '5px';
+  cherryBtn.style.backgroundColor = 'rgb(246, 248, 250)';
+  cherryBtn.style.margin = '0 1rem';
+  cherryBtn.style.width = '28px';
+  cherryBtn.style.height = '28px';
+
+  cherryBtn.innerText = `🍒`;
+  cherryBtn.onclick = () => {
+    navigator.clipboard.writeText(`git cherry-pick ${commitSHA}`).then(
+      () => {
+        // Successful copy to  clipboard.
+      },
+      () => {
+        alert('Failed to copy SHA');
+      }
+    );
+  };
+
+  return cherryBtn;
+};
 
 commitItems.forEach(commitItem => {
-  const test = document.createElement('button');
-  test.innerText = "Test";
-  commitItem.appendChild(test);
-});
+  const liElem = commitItem as HTMLLIElement;
+  const btn = createBtn(liElem);
 
-///KristofferLinderman/git-quick/commits/b28366ec34dcc69af33671bdb74b4bea86b2bc1c/commits_list_item
+  liElem.style.alignItems = 'center';
+  liElem.append(btn);
+});
