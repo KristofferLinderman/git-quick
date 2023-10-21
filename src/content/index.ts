2211191c60ced4ed56d37bcf01bb31171f9443fa
Kristoffer Linderman <kristoffer.linderman@bygghemma.se>
2023-10-21 16:19:24 +0200

    Basic Cherry btn complete

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,7 +1,28 @@
 const commitItems = document.querySelectorAll('.TimelineItem-body > ol > li');
 
+const copySHA = (cherryBtn: HTMLButtonElement, commitSHA: string) => {
+  navigator.clipboard.writeText(`git cherry-pick ${commitSHA}`).then(
+    () => {
+      // Successful copy to  clipboard.
+      cherryBtn.innerHTML =
+        '<svg style="display: inline-block;" aria-hidden="false" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check color-fg-success"><path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path></svg>';
+
+      setTimeout(() => {
+        cherryBtn.innerText = `🍒`;
+      }, 1000);
+    },
+    () => {
+      alert('Failed to copy SHA');
+    }
+  );
+};
+
 const createBtn = (commitItem: HTMLLIElement) => {
   const commitSHA = commitItem.dataset.url?.split('commits/')[1].split('/')[0];
+  if (!commitSHA) {
+    return;
+  }
+
   const cherryBtn = document.createElement('button');
 
   cherryBtn.style.padding = '0';
@@ -13,22 +34,7 @@ const createBtn = (commitItem: HTMLLIElement) => {
   cherryBtn.style.height = '28px';
 
   cherryBtn.innerText = `🍒`;
-  cherryBtn.onclick = () => {
-    navigator.clipboard.writeText(`git cherry-pick ${commitSHA}`).then(
-      () => {
-        // Successful copy to  clipboard.
-        cherryBtn.innerHTML =
-          '<svg style="display: inline-block;" aria-hidden="false" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check color-fg-success"><path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path></svg>';
-
-        setTimeout(() => {
-          cherryBtn.innerText = `🍒`;
-        }, 1000);
-      },
-      () => {
-        alert('Failed to copy SHA');
-      }
-    );
-  };
+  cherryBtn.onclick = () => copySHA(cherryBtn, commitSHA);
 
   return cherryBtn;
 };
@@ -37,6 +43,8 @@ commitItems.forEach(commitItem => {
   const liElem = commitItem as HTMLLIElement;
   const btn = createBtn(liElem);
 
-  liElem.style.alignItems = 'center';
-  liElem.append(btn);
+  if (btn) {
+    liElem.style.alignItems = 'center';
+    liElem.append(btn);
+  }
 });
