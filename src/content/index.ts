5ff13b58026a05993ed1c009b6519998884464bc
Kristoffer Linderman <kristoffer.linderman@bygghemma.se>
2023-10-21 16:14:20 +0200

    Add checkmark on copy

--- src/content/index.ts
+++ src/content/index.ts
@@ -17,6 +17,12 @@ const createBtn = (commitItem: HTMLLIElement) => {
     navigator.clipboard.writeText(`git cherry-pick ${commitSHA}`).then(
       () => {
         // Successful copy to  clipboard.
+        cherryBtn.innerHTML =
+          '<svg style="display: inline-block;" aria-hidden="false" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check color-fg-success"><path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path></svg>';
+
+        setTimeout(() => {
+          cherryBtn.innerText = `🍒`;
+        }, 1000);
       },
       () => {
         alert('Failed to copy SHA');
