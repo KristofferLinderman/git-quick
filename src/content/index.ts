e6dcc3ced62d34b0bc4eb68f2fe9b76a1e44cf04
Kristoffer <Kristoffermglinderman@gmail.com>
2023-11-05 20:15:12 +0100

    Fix reading init state + honor settings + clean-up

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,22 +1,51 @@
-const commitItems = document.querySelectorAll('.TimelineItem-body > ol > li');
-
 type BtnIcons = '🍒' | '🔄';
+type BTN_TITLES = 'Cherry-Pick' | 'Revert';
 
 const WRAPPER_ID = 'git-quick-wrapper';
 
-const copySHA = (
-  cherryBtn: HTMLButtonElement,
+let shouldShowCherry: boolean = false;
+let shouldShowRevert: boolean = false;
+let previousUrl: string = '';
+let isRunning = false;
+let hasWrapper = false;
+
+// Get settings and listen to changes
+chrome.storage.sync.get('cherry', data => {
+  shouldShowCherry = !!data['cherry'];
+});
+
+chrome.storage.sync.get('revert', data => {
+  shouldShowRevert = !!data['revert'];
+});
+
+const handleStorageChange = (event: {
+  [key: string]: chrome.storage.StorageChange;
+}) => {
+  if (Object.keys(event).includes('cherry')) {
+    shouldShowCherry = event.cherry.newValue;
+    renderButtons();
+  } else if (Object.keys(event).includes('revert')) {
+    shouldShowRevert = event.revert.newValue;
+    renderButtons();
+  }
+};
+
+chrome.storage.onChanged.addListener(handleStorageChange);
+
+const copyGitCommad = (
+  button: HTMLButtonElement,
   commitSHA: string,
-  btnIcon: BtnIcons
+  btnIcon: BtnIcons,
+  gitCommand: string
 ) => {
-  navigator.clipboard.writeText(`git cherry-pick ${commitSHA}`).then(
+  navigator.clipboard.writeText(`git ${gitCommand} ${commitSHA}`).then(
     () => {
       // Successful copy to  clipboard.
-      cherryBtn.innerHTML =
+      button.innerHTML =
         '<svg style="display: inline-block;" aria-hidden="false" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check color-fg-success"><path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path></svg>';
 
       setTimeout(() => {
-        cherryBtn.innerText = btnIcon;
+        button.innerText = btnIcon;
       }, 1000);
     },
     () => {
@@ -25,30 +54,35 @@ const copySHA = (
   );
 };
 
-const createBtn = (commitSHA: string, icon: BtnIcons, title: string) => {
-  const cherryBtn = document.createElement('button');
-  cherryBtn.id = `${title}-${commitSHA}`;
+const createBtn = (commitSHA: string, icon: BtnIcons, title: BTN_TITLES) => {
+  const button = document.createElement('button');
+  button.id = `${title}-${commitSHA}`;
 
-  cherryBtn.style.padding = '0';
-  cherryBtn.style.border = '1px solid rgba(31, 35, 40, 0.15)';
-  cherryBtn.style.borderRadius = '5px';
-  cherryBtn.style.backgroundColor = 'rgb(246, 248, 250)';
-  cherryBtn.style.width = '28px';
-  cherryBtn.style.height = '28px';
+  button.style.padding = '0';
+  button.style.border = '1px solid rgba(31, 35, 40, 0.15)';
+  button.style.borderRadius = '5px';
+  button.style.backgroundColor = 'rgb(246, 248, 250)';
+  button.style.width = '28px';
+  button.style.height = '28px';
 
-  cherryBtn.title = title;
-  cherryBtn.innerText = icon;
-  cherryBtn.onclick = () => copySHA(cherryBtn, commitSHA, icon);
+  button.title = title;
+  button.innerText = icon;
 
-  return cherryBtn;
+  switch (title) {
+    case 'Cherry-Pick':
+      button.onclick = () =>
+        copyGitCommad(button, commitSHA, icon, 'cherry-pick');
+      break;
+    case 'Revert':
+      button.onclick = () => copyGitCommad(button, commitSHA, icon, 'revert');
+      break;
+  }
+
+  return button;
 };
 
 const CreateWrapper = (wrapperId: string) => {
   const existingWrapper = document.getElementById(wrapperId);
-  console.log(
-    '🐼 ~ file: index.ts:48 ~ CreateWrapper ~ existingWrapper:',
-    existingWrapper
-  );
   if (existingWrapper) {
     existingWrapper.innerHTML = '';
     return existingWrapper;
@@ -67,47 +101,22 @@ const CreateWrapper = (wrapperId: string) => {
   return wrapper;
 };
 
-let shouldShowCherry: boolean = false;
-let shouldShowRevert: boolean = false;
+const renderButtons = () => {
+  if (!shouldShowCherry && !shouldShowRevert) {
+    const allWrappers = document.querySelectorAll(`[id^='git-quick-wrapper']`);
 
-chrome.storage.sync.get('cherry', data => {
-  shouldShowCherry = !!data;
-});
-chrome.storage.sync.get('revert', data => {
-  shouldShowRevert = !!data;
-});
-
-const handleStorageChange = (event: {
-  [key: string]: chrome.storage.StorageChange;
-}) => {
-  if (Object.keys(event).includes('cherry')) {
-    shouldShowCherry = event.cherry.newValue;
-    console.log('🐼 ~ file: index.ts:76 ~ shouldShowCherry:', shouldShowCherry);
-    renderButtons();
-  } else if (Object.keys(event).includes('revert')) {
-    shouldShowRevert = event.revert.newValue;
-    console.log('🐼 ~ file: index.ts:80 ~ shouldShowRevert:', shouldShowRevert);
-    renderButtons();
-  }
-};
-chrome.storage.onChanged.addListener(handleStorageChange);
-
-const main = () => {
-  isRunning = true;
-  if (document.getElementById('git-quick-wrapper')) {
+    allWrappers.forEach(element => {
+      element.remove();
+    });
     return;
   }
-  renderButtons();
-};
 
-const renderButtons = () => {
   const commitItems = document.querySelectorAll('.TimelineItem-body > ol > li');
-
   commitItems.forEach(commitItem => {
     const liElem = commitItem as HTMLLIElement;
     const commitSHA = liElem.dataset.url?.split('commits/')[1].split('/')[0];
 
-    if (!commitSHA || (!shouldShowCherry && !shouldShowRevert)) {
+    if (!commitSHA) {
       return;
     }
 
@@ -139,10 +148,7 @@ const renderButtons = () => {
   });
 };
 
-let previousUrl: string = '';
-let isRunning = false;
-let hasWrapper = false;
-
+// Check url changes
 setInterval(() => {
   const currentURL = window.location.href;
   if (currentURL === previousUrl) {
@@ -154,3 +160,11 @@ setInterval(() => {
     main();
   }
 }, 200);
+
+const main = () => {
+  isRunning = true;
+  if (document.getElementById('git-quick-wrapper')) {
+    return;
+  }
+  renderButtons();
+};
